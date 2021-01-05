4e0f8f27ee24d09e651adb26528a952a3f2b39a4
Richard Williamson <richard@nilfactor.com>
2021-01-05 12:28:23 -0600

    fix for twitch

--- src/components/TwitchPlr.jsx
+++ src/components/TwitchPlr.jsx
@@ -52,7 +52,7 @@ class TwitchPlr extends PureComponent {
             width: 1400,
             height: 600,
             channel: window.stream,
-            parent: [window.location.hostname]
+            parent: [`http://window.location.hostname`]
         });
         const tim = setTimeout(() => {
             const list = document.getElementsByClassName('tw-core-button--primary');
