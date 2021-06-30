9c901220d6fa3d80273b4e4daa1c24bf7714a38a
Richard Williamson <richard.williamson@fieldnation.com>
2021-06-30 14:19:37 -0500

    added to hypehorizen

--- chat-commands.js
+++ chat-commands.js
@@ -13,4 +13,33 @@ module.exports = [
         },
         coolDown: 0,
     },
+    /* @HypeHorizen Hype Rizing Team */
+    {
+        command: '!hyperizing',
+        cb: (client, params, target) => {
+            client.say(target, 'Join the Hype Horizen Community Team by visiting https://hypehorizen.com/hype-rizing');
+        },
+        coolDown: 0,
+    },
+    {
+        command: '!hypehorizen',
+        cb: (client, params, target) => {
+            client.say(target, 'Get hyped up! Hype Horizen is an awesome team of players and streamers, check out my team and cheer us on https://hypehorizen.com');
+        },
+        coolDown: 0,
+    },
+    {
+        command: '!fifine',
+        cb: (client, params, target) => {
+            client.say(target, 'Hype Rizing and Hype Horizen is sponsored by FIFINE Technology https://hypehorizen.com/fifinerizing a company that specializes in giving streamers like me affordable, quality audio products and microphones')
+        },
+        coolDown: 0,
+    },
+    {
+        command: '!shop',
+        cb: (client, params, target) => {
+            client.say(target, 'Get Hype Merch! https://hypehorizen.com/shop My team has tees, hoodies and merch available for gamers')
+        },
+        coolDown: 0,
+    },
 ];
