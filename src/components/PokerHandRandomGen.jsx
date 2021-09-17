0bf36f94d693498e8599489ab7800711db14be11
Richard Williamson <richard.williamson@fieldnation.com>
2021-09-17 00:16:51 -0500

    adding poker thing

---
+++ src/components/PokerHandRandomGen.jsx
@@ -0,0 +1,42 @@
+import React, { PureComponent } from 'react';
+
+export default class PokerHandRandomGen extends PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            number: 0,
+            color: 'red',
+        };
+    }
+
+    generateNumber() {
+        const number = Math.floor(Math.random() * 100);
+        let color = 'red';
+        if (number >= 75) {
+            color = 'green';
+        } else if (number >= 50) {
+            color = 'orange';
+        }
+
+        this.setState({
+            number,
+            color,
+        });
+    }
+
+    render() {
+        const {
+            color,
+            number
+        } = this.state;
+        return (
+            <div style={{
+                backgroundColor: color,
+                border: '1px solid #222',
+                color: 'white' }}
+            >
+                <h1>{number}</h1>
+            </div>
+        );
+    }
+}
